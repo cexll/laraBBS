4cc7eb94e0b15afd9f3edc835635598d8c9cd67b
cexll <evanxian9@gmail.com>
2020-07-11 21:16:24 +0800

    使用 JWT

--- app/Http/Controllers/Api/AuthorizationsController.php
+++ app/Http/Controllers/Api/AuthorizationsController.php
@@ -2,11 +2,13 @@
 
 namespace App\Http\Controllers\Api;
 
+use App\Http\Requests\Api\AuthorizationRequest;
 use App\Http\Requests\Api\SocialAuthorizationRequest;
 use App\Models\User;
 use Illuminate\Auth\AuthenticationException;
 use Illuminate\Http\Request;
 use Illuminate\Support\Arr;
+use Illuminate\Support\Facades\Auth;
 use Laravel\Socialite\Facades\Socialite;
 
 class AuthorizationsController extends Controller
@@ -55,6 +57,46 @@ class AuthorizationsController extends Controller
                 break;
         }
 
-        return response()->json(['token' => $user->id]);
+        $token = auth('api')->login($user);
+
+        return $this->respondwithToken($token)->setStatusCode(201);
+    }
+
+    public function store(AuthorizationRequest $request)
+    {
+        $username= $request->username;
+
+        filter_var($username, FILTER_VALIDATE_EMAIL) ?
+            $credentials['email'] = $username :
+            $credentials['phone'] = $username;
+
+        $credentials['password'] = $request->password;
+
+        if (!$token = Auth::guard('api')->attempt($credentials)) {
+            throw new AuthenticationException('用户名或密码错误');
+        }
+
+        return $this->respondwithToken($token)->setStatusCode(201);
+    }
+
+    public function update()
+    {
+        $token = auth('api')->refresh();
+        return $this->respondwithToken($token);
+    }
+
+    public function destroy()
+    {
+        auth('api')->logout();
+        return response(null, 204);
+    }
+
+    public function respondwithToken($token)
+    {
+        return response()->json([
+            'access_token' => $token,
+            'token_type' => 'Bearer',
+            'expires_in' => auth('api')->factory()->getTTL() * 60
+        ]);
     }
 }
