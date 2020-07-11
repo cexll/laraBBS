028def01954ce3de0ec73d5a3ee32cbf8c137c3f
evanxian <17772300530@163.com>
2020-07-11 18:46:49 +0800

    用户注册

--- app/Http/Controllers/Api/VerificationCodesController.php
+++ app/Http/Controllers/Api/VerificationCodesController.php
@@ -2,6 +2,9 @@
 
 namespace App\Http\Controllers\Api;
 
+use AlibabaCloud\Client\AlibabaCloud;
+use AlibabaCloud\Client\Exception\ClientException;
+use AlibabaCloud\Client\Exception\ServerException;
 use Illuminate\Support\Facades\Cache;
 use Illuminate\Support\Str;
 use App\Http\Requests\Api\VerificationCodeRequest;
@@ -12,24 +15,42 @@ class VerificationCodesController extends Controller
 {
     public function store(VerificationCodeRequest $request, EasySms $easySms)
     {
+        AlibabaCloud::accessKeyClient(config('easysms.gateways.aliyun.access_key_id'), config('easysms.gateways.aliyun.access_key_secret'))->regionId('cn-hangzhou')->asDefaultClient();
+
         $phone = $request->phone;
 
-        // 生成4位随机数，左侧补0
-        $code = str_pad(random_int(1, 9999), 4, 0, STR_PAD_LEFT);
+        if (!app()->environment('production')) {
+            $code = '1234';
+        } else {
+            // 生成4位随机数，左侧补0
+            $code = str_pad(random_int(1, 9999), 4, 0, STR_PAD_LEFT);
 
-        try {
-            $result = $easySms->send($phone, [
-                'template' => config('easysms.gateways.aliyun.templates.register'),
-                'data' => [
-                    'code' => $code
-                ],
-            ]);
-            var_dump($result);
-
-        } catch (\Overtrue\EasySms\Exceptions\NoGatewayAvailableException $exception) {
-            $message = $exception->getException('aliyun');
-            abort(500, $message ?: '短信发送异常');
+            try {
+                $result = AlibabaCloud::rpc()
+                    ->product('Dysmsapi')
+                    // ->scheme('https') // https | http
+                    ->version(date('Y-m-d'))
+                    ->action('SendSms')
+                    ->method('POST')
+                    ->host('dysmsapi.aliyuncs.com')
+                    ->options([
+                        'query' => [
+                            'RegionId' => "cn-hangzhou",
+                            'PhoneNumbers' => $phone,
+                            'SignName' => config('easysms.gateways.aliyun.sign_name'),
+                            'TemplateCode' => config('easysms.gateways.aliyun.templates.register'),
+                            'TemplateParam' => "{\"code\":\"$code\"}",
+                        ],
+                    ])
+                    ->request();
+//                print_r($result->toArray());
+            } catch (ClientException $e) {
+                abort(500, $e->getErrorMessage());
+            } catch (ServerException $e) {
+                abort(500, $e->getErrorMessage());
+            }
         }
+
 
         $key = 'verificationCode_'.Str::random(15);
         $expiredAt = now()->addMinutes(5);
