c12bafe8861de99002ff13b0f98f82fcd7821500
evanxian <17772300530@163.com>
2020-07-11 18:20:53 +0800

    发送短信验证码

--- routes/api.php
+++ routes/api.php
@@ -1,7 +1,7 @@
 <?php
 
 use Illuminate\Http\Request;
-
+use \Illuminate\Support\Facades\Route;
 /*
 |--------------------------------------------------------------------------
 | API Routes
@@ -21,5 +21,14 @@ Route::prefix('v1')
        // 短信验证码
         Route::post('verificationCodes', 'VerificationCodesController@store')
             ->name('verificationCodes.store');
+        // 用户注册
+        Route::post('users', 'UsersController@store')
+            ->name('users.store');
+        // 第三方登录
+        Route::post('socials/{social_type}/authorizations',
+        'AuthorizationsController@socialStore')
+            ->where('social_type', 'weixin')
+            ->name('socials.authorizations.store');
+
 });
 
