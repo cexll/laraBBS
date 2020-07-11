fa7aac98213f557c46b9a56b1bfb3150d4630c7d
evanxian <17772300530@163.com>
2020-07-11 18:53:03 +0800

    频率限制

--- routes/api.php
+++ routes/api.php
@@ -14,21 +14,30 @@ use \Illuminate\Support\Facades\Route;
 */
 
 
-Route::prefix('v1')
-    ->namespace('Api')
-    ->name('api.v1.')
+Route::prefix('v1')->namespace('Api')->name('api.v1.')
     ->group(function () {
-       // 短信验证码
-        Route::post('verificationCodes', 'VerificationCodesController@store')
-            ->name('verificationCodes.store');
-        // 用户注册
-        Route::post('users', 'UsersController@store')
-            ->name('users.store');
-        // 第三方登录
-        Route::post('socials/{social_type}/authorizations',
-        'AuthorizationsController@socialStore')
-            ->where('social_type', 'weixin')
-            ->name('socials.authorizations.store');
+
+        Route::middleware('throttle:'.config('api.rate_limits.sign'))
+            ->group(function () {
+                // 短信验证码
+                Route::post('verificationCodes', 'VerificationCodesController@store')
+                    ->name('verificationCodes.store');
+                // 用户注册
+                Route::post('users', 'UsersController@store')
+                    ->name('users.store');
+                // 第三方登录
+                Route::post('socials/{social_type}/authorizations',
+                    'AuthorizationsController@socialStore')
+                    ->where('social_type', 'weixin')
+                    ->name('socials.authorizations.store');
+            });
+
+        Route::middleware('throttle:'. config('api_rate_limits.access'))
+            ->group(function () {
+
+            });
+
+
 
 });
 
