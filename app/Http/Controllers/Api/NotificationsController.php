255b37e37818a5c4101d5e20ab0c7112c5aa35f4
evanxian <17772300530@163.com>
2020-07-12 16:36:53 +0800

    消息通知统计

--- app/Http/Controllers/Api/NotificationsController.php
+++ app/Http/Controllers/Api/NotificationsController.php
@@ -6,10 +6,27 @@ use Illuminate\Http\Request;
 
 class NotificationsController extends Controller
 {
+    /**
+     * 消息通知列表
+     * @param Request $request
+     * @return \Illuminate\Http\Resources\Json\AnonymousResourceCollection
+     */
     public function index(Request $request)
     {
         $notifications  = $request->user()->notifications()->paginate();
 
         return NotificationResource::collection($notifications);
+    }
+
+    /**
+     * 消息列表数量
+     * @param Request $request
+     * @return \Illuminate\Http\JsonResponse
+     */
+    public function stats(Request $request)
+    {
+        return response()->json([
+            'unread_count' => $request->user()->notification_count,
+        ]);
     }
 }
